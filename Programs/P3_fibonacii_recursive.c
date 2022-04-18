e16956d3867ba1c13633252093d64f82388be581
Jakub Nowak <nowakkuba99@gmail.com>
2022-04-18 14:44:08 +0200

    Added program to calculate fibonacci sequence

---
+++ Programs/P3_fibonacii_recursive.c
@@ -0,0 +1,30 @@
+/* ⓒ Jakub Nowak 
+Program to display fibonacii sequence with recursive method
+Arguments: Amount of numbers to display
+Return: -
+*/
+
+/*Includes*/
+#include <stdio.h>
+/*File includes*/
+int fibonacci(int);
+/*Functions definitions*/
+
+/*Main*/
+int main()
+{
+    printf("%d",fibonacci(50));
+    return 1;
+}
+/*Functions bodies*/
+int fibonacci(int num)
+{
+    if(num <= 1)
+    {
+        return num;   
+    }
+    else
+    {
+        return (fibonacci(num-1) + fibonacci(num-2));
+    }
+}
