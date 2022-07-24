de74dc78d564258284c68deccdf16ec22d28b1f8
Jakub Nowak <nowakkuba99@gmail.com>
2022-07-24 17:55:15 +0200

    MaximumDepthOfBinaryTree C++

---
+++ Blind 75 - C++/Trees/MaximumDepthOfBinaryTree.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ **/
+#define MAX(x,y) (((x)>(y))?(x):(y))
+#include<algorithm>
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr)
+            return 0;
+        else
+            return std::max(maxDepth(root->left),maxDepth(root->right))+1;
+    }
+};
